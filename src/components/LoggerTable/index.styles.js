b114bbab379b97400eaa23b7b43946d63b708469
valerii.kocheharov <valerii.kocheharov@anoda.mobi>
2022-04-27 11:55:22 +0300

    Added logs table  page

---
+++ src/components/LoggerTable/index.styles.js
@@ -0,0 +1,44 @@
+import { createStyles } from "@mantine/core";
+import styled from "styled-components";
+
+export const useStyles = createStyles((theme) => ({
+  form: {
+    minHeight: "100vh",
+  },
+  header: {
+    position: "sticky",
+    top: 0,
+    backgroundColor: theme.colors.dark[7],
+    transition: "box-shadow 150ms ease",
+    color: "rgb(193, 194, 197)",
+
+    "&::after": {
+      content: '""',
+      position: "absolute",
+      left: 0,
+      right: 0,
+      bottom: 0,
+      borderBottom: `1px solid ${theme.colors.dark[3]}`,
+    },
+  },
+
+  scrolled: {
+    boxShadow: theme.shadows.sm,
+  },
+  body: {
+    color: "rgb(193, 194, 197)",
+  },
+  headerText: {
+    color: "rgb(255, 212, 59)",
+    fontSize: 18,
+  },
+  rowText: {
+    fontSize: 16,
+  },
+}));
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+`;
