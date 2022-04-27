c8756dcec37b85174839b31d1adaa3cc10ec116a
valerii.kocheharov <valerii.kocheharov@anoda.mobi>
2022-04-27 14:50:32 +0300

    Added protected routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,13 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Project from "../pages/Project";
+import ProtectedRoute from "./ProtectedRoute";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />}></Route>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/projects/:projectName" element={<Project />}></Route>
+      <Route path="/" element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+      <Route path="/projects/:projectName" element={<ProtectedRoute />}>
+        <Route path="/projects/:projectName" element={<Project />} />
+      </Route>
     </Routes>
   );
 };
